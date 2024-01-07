a0fef0b3effe8c37d1c9740c6b0315865d800974
wangzeng <2502101454@qq.com>
2024-01-07 10:44:07 +0800

    commit current files

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author zengwang
+ * @create 2024-01-07 10:35
+ * @desc:
+ */
+object Test {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+    var a: Int = 1
+  }
+
+}
